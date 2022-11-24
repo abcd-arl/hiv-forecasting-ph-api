238fc48fd17f060a104b5496700736b6c64d9582
abcd-arl <lingga.abdulrahman.u@gmail.com>
2022-11-25 06:30:59 +0800

    configure aws s3

--- forecasting/settings.py
+++ forecasting/settings.py
@@ -48,7 +48,8 @@ INSTALLED_APPS = [
     'rest_framework',
     "rest_framework.authtoken",
     "dj_rest_auth",
-    'apis.apps.ApisConfig'
+    'apis.apps.ApisConfig',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -131,6 +132,12 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = env.str("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = env.str("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = env.str("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_FILE_OVERWRITE = False
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
@@ -153,3 +160,5 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]
+
+
