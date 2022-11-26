104e275dfbcf58d7707cfec94dd3c1eec002751c
abcd-arl <lingga.abdulrahman.u@gmail.com>
2022-11-27 03:28:14 +0800

    configure aws rds

--- forecasting/settings.py
+++ forecasting/settings.py
@@ -89,7 +89,14 @@ WSGI_APPLICATION = 'forecasting.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': env.dj_db_url("DATABASE_URL")
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': env.str("DB_NAME"),
+    'USER': env.str("DB_USER"),
+    'PASSWORD': env.str("DB_PASSWORD"),
+    'HOST': env.str("DB_HOST"),
+    'PORT': env.str("DB_PORT"),
+  }
 }
 
 
