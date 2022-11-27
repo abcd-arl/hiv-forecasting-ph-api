bb50057f3029a1208ccc7f950be50c94e37ae5cd
abcd-arl <lingga.abdulrahman.u@gmail.com>
2022-11-27 18:41:00 +0800

    add host

--- forecasting/settings.py
+++ forecasting/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1", "35.93.57.77:8000"]
 
 CSRF_TRUSTED_ORIGINS = ['https://8000-abcdarl-hivforecastingd-4p274ahpjtp.ws-us67.gitpod.io']
 
