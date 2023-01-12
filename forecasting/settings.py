24d8a9e364ab00561883c53693c8e7479b394a59
abcd-arl <lingga.abdulrahman.u@gmail.com>
2023-01-12 11:54:28 +0800

    update

--- forecasting/settings.py
+++ forecasting/settings.py
@@ -88,15 +88,22 @@ WSGI_APPLICATION = 'forecasting.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#   'default': {
+#     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#     'NAME': env.str("DB_NAME"),
+#     'USER': env.str("DB_USER"),
+#     'PASSWORD': env.str("DB_PASSWORD"),
+#     'HOST': env.str("DB_HOST"),
+#     'PORT': env.str("DB_PORT"),
+#   }
+# }
+
 DATABASES = {
-  'default': {
-    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'NAME': env.str("DB_NAME"),
-    'USER': env.str("DB_USER"),
-    'PASSWORD': env.str("DB_PASSWORD"),
-    'HOST': env.str("DB_HOST"),
-    'PORT': env.str("DB_PORT"),
-  }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
